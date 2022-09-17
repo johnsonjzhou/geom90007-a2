ea8ae6b0cc8a5f5f476e4e945af8bc37522bb057
Johnson Zhou <johnson@simplyuseful.io>
2022-09-17 21:20:36 +1000

    chore: build javascript with webpack

--- webpack.js
+++ webpack.js
@@ -1,19 +1,23 @@
-'use strict';
-const webpack = require('webpack');
-const path = require('path');
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+"use strict";
+const webpack = require("webpack");
+const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    'shiny': [ './src/scss/_build.scss' ]
+    "shiny_app": [
+      "./src/scss/_build.scss",
+      "./src/js/_build.js"
+    ]
   },
   output: {
-    path: path.resolve('./www/'),
+    path: path.resolve("./www/"),
     clean: true
   },
-  mode: 'production',
-  devtool: 'source-map',
+  mode: "production",
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -23,26 +27,34 @@ module.exports = {
             loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               sourceMap: true,
               url: false,
             },
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
-              implementation: require('sass')
+              implementation: require("sass")
             }
           }
         ]
-      }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
     ]
   },
   plugins: [
-    // shiny.css
+    // delete existing packed assets
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["./www"]
+    }),
+    // minify CSS
     new MiniCssExtractPlugin(),
-
     // clean up
     new RemoveEmptyScriptsPlugin(),
   ]
