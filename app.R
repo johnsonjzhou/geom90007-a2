3b6d0219f0713becd7f83fe56fe3eccd69976e06
Johnson Zhou <johnson@simplyuseful.io>
2022-09-17 20:20:58 +1000

    feat: app instructions

--- app.R
+++ app.R
@@ -4,6 +4,8 @@
 # @author Johnson Zhou zhoujj@student.unimelb.edu.au                           #
 ################################################################################
 
-library(shiny)
+# Please run the following to ensure dependencies are installed
+source("./R/libraries.R")
 
+# Define the Shiny app
 shiny::shinyApp(ui, server)
